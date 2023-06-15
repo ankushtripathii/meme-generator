f301ec13021e08d6c5271d5074e628c9bee11819
Ankush Tripathi <89534251+ankushtripathii@users.noreply.github.com>
2023-06-15 15:54:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,13 @@ const memeImg = document.querySelector(".memeimg");
 const memeTitl = document.querySelector(".meme_title");
 
 //generate meme
-const updateDetail = (url, title) => {
-  memeImg.setAttribute("src", url);
-  memeTitl.innerHTML = title;
-};
+
 const generateMeme = () => {
   fetch("https://meme-api.com/gimme/wholesomememes")
     .then((resp) => resp.json())
     .then((data) => {
-      updateDetail(data.url, data.title);
+      memeImg.setAttribute("src", data.url);
+      memeTitl.innerHTML = data.title;
     });
 };
 
